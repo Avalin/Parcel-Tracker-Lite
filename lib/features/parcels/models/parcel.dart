72c109ac93dd8e5612a15d1b1f6bbafaf2b9dc38
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2026-04-12 14:59:27 +0200

    Added delivery status to model

--- lib/features/parcels/models/parcel.dart
+++ lib/features/parcels/models/parcel.dart
@@ -1,11 +1,13 @@
 class Parcel {
   final String id;
   final String trackingNumber;
+  final String deliveryStatus;
   final String postCode;
 
   const Parcel({
     required this.id,
     required this.trackingNumber,
+    required this.deliveryStatus,
     required this.postCode,
   });
 }
