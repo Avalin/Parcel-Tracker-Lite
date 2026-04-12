6feb4b93b6737f854ff002dc5afff9e09c678fa0
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2026-04-12 16:31:40 +0200

    Used the tiny tracker, now we get randomized statuses on create

--- lib/features/parcels/controllers/parcels_controller.dart
+++ lib/features/parcels/controllers/parcels_controller.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'package:parcel_tracker_lite/features/parcels/enums/parcel_status.dart';
 import 'package:parcel_tracker_lite/features/parcels/models/parcel.dart';
 import 'package:parcel_tracker_lite/features/parcels/services/parcel_storage_service.dart';
+import 'package:parcel_tracker_lite/features/parcels/services/tracking_service.dart';
 
 class ParcelsController extends ChangeNotifier {
   ParcelsController({
     ParcelStorageService? storageService,
-  }) : _storageService = storageService ?? ParcelStorageService();
+    TrackingService? trackingService,
+  }) : _storageService = storageService ?? ParcelStorageService(),
+       _trackingService = trackingService ?? TrackingService();
 
   final ParcelStorageService _storageService;
+  final TrackingService _trackingService;
 
   final List<Parcel> _parcels = [];
 
@@ -57,13 +60,16 @@ class ParcelsController extends ChangeNotifier {
     notifyListeners();
 
     try {
-      await Future.delayed(const Duration(milliseconds: 500));
+      final fetchedStatus = await _trackingService.fetchStatus(
+        trackingNumber: trimmedTrackingNumber,
+        postcode: postCode,
+      );
 
       final parcel = Parcel(
         id: DateTime.now().millisecondsSinceEpoch.toString(),
         trackingNumber: trimmedTrackingNumber,
         postCode: postCode,
-        parcelStatus: ParcelStatus.created
+        parcelStatus: fetchedStatus
       );
 
       _parcels.insert(0, parcel);
