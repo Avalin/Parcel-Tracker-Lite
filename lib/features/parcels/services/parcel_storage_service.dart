3b1f9986cccc15afda57c3fac566e85d367851c5
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2026-04-12 16:13:01 +0200

    Added parcel storage service for stored parcels

---
+++ lib/features/parcels/services/parcel_storage_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:parcel_tracker_lite/features/parcels/models/parcel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// We use Shared Preferences here instead of Hive or Drift in this case, because limited time :D
+class ParcelStorageService {
+  static const String _parcelsKey = 'parcels';
+
+  Future<List<Parcel>> loadParcels() async {
+    final preferences = await SharedPreferences.getInstance();
+    final parcelStrings = preferences.getStringList(_parcelsKey);
+
+    if (parcelStrings == null || parcelStrings.isEmpty) {
+      return [];
+    }
+
+    return parcelStrings
+        .map((parcelString) => Parcel.fromJson(jsonDecode(parcelString) as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> saveParcels(List<Parcel> parcels) async {
+    final preferences = await SharedPreferences.getInstance();
+
+    final parcelStrings = parcels
+        .map((parcel) => jsonEncode(parcel.toJson()))
+        .toList();
+
+    await preferences.setStringList(_parcelsKey, parcelStrings);
+  }
+}
