053bd8019c3827e978e81a2a63f00885dd171a58
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2026-04-12 14:52:44 +0200

    Added very basic immutable Parcel class and cleaned out counter logic in home page

---
+++ lib/features/parcels/models/parcel.dart
@@ -0,0 +1,11 @@
+class Parcel {
+  final String id;
+  final String trackingNumber;
+  final String postCode;
+
+  const Parcel({
+    required this.id,
+    required this.trackingNumber,
+    required this.postCode,
+  });
+}
